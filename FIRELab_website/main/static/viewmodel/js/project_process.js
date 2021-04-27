506e012f5d263432e8b1ee9db28dfb4cc0db2b0a
Mariana Ladeiro <marianaladeiro@ua.pt>
2021-04-27 13:38:21 +0100

    fixes

--- FIRELab_website/main/static/viewmodel/js/project_process.js
+++
@@ -1,6 +0,0 @@
-//to make entire row clickable
-//$('tr').click( function() {
-  //  window.location = $(this).find('a').attr('href');
-//}).hover( function() {
-  //  $(this).toggleClass('hover');
-//});
