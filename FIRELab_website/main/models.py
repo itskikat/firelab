49a5bda1238e0e1fd13cb0d7d0df0436b96c7943
davidgmorais <david_morais35@live.com.pt>
2021-05-03 14:20:16 +0100

    Update models.py

--- FIRELab_website/main/models.py
+++ FIRELab_website/main/models.py
@@ -37,18 +37,10 @@ class Directory(MPTTModel):
         return '~' + path_str
 
 
-class FileType(models.Model):
-    type = models.CharField(max_length=30)
-
-    def __str__(self):
-        return str(self.id) + " - " + self.type
-
-
 class FileInfo(models.Model):
     name = models.CharField(max_length=50)
     extension = models.CharField(max_length=10)
     dir = models.ForeignKey(Directory, on_delete=models.CASCADE)
-    type_id = models.ForeignKey(FileType, on_delete=models.CASCADE)
 
     def __str__(self):
         full_name = self.name
@@ -58,34 +50,6 @@ class FileInfo(models.Model):
 
     def get_path(self):
         return self.dir.get_path() + "/" + self.name + "." + self.extension
-
-
-def image_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/frames/<video_name>/
-    return 'images/p{}_{}'.format(instance.file_info.dir.project.id, filename)
-
-
-class Image(models.Model):
-    content = models.ImageField(upload_to=image_path, blank=False)
-    file_info = models.OneToOneField(FileInfo, on_delete=models.CASCADE, blank=False)
-
-    def __str__(self):
-        return "{} - Image ({})".format(self.id, self.get_filename())
-
-    def get_filename(self):
-        return "{}.{}".format(self.file_info.name, self.file_info.extension)
-
-
-class Mask(models.Model):
-    content = models.BinaryField(blank=False)
-    file_info = models.OneToOneField(FileInfo, on_delete=models.CASCADE, blank=False)
-    image_id = models.ForeignKey(Image, on_delete=models.CASCADE, blank=False)
-
-    def __str__(self):
-        return "{} - Mask ({})".format(self.id, self.get_filename())
-
-    def get_filename(self):
-        return "{}.{}".format(self.file_info.name, self.file_info.extension)
 
 
 class Video(models.Model):
@@ -106,15 +70,22 @@ def video_delete(sender, instance, **kwargs):
         instance.content.delete(False)
 
 
-def video_frame_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/frames/<video_name>/
-    return 'frames/{}/p{}_{}'.format(instance.video.name, instance.file_info.dir.project.id, filename)
+def image_path(instance, filename):
+    if instance.video is not None:
+        # video frame will be uploaded to MEDIA_ROOT/frames/<video_name>/
+        return 'frames/{}/p{}_{}'.format(instance.video.name, instance.file_info.dir.project.id, filename)
+    else:
+        # image will be uploaded to  MEDIA_ROOT/images/
+        return 'images/p{}_{}'.format(instance.file_info.dir.project.id, filename)
 
 
-class Frame(models.Model):
-    video = models.ForeignKey(Video, on_delete=models.CASCADE, blank=False)
-    content = models.ImageField(upload_to=video_frame_path, blank=False)
+class ImageFrame(models.Model):
+    content = models.ImageField(upload_to=image_path, blank=False)
     file_info = models.OneToOneField(FileInfo, on_delete=models.CASCADE, blank=False)
+    mask = models.BinaryField(blank=True, null=True, default=None)
+    polygon = models.CharField(max_length=100, blank=True, null=True, default=None)  # change to postGis polygon
+    # add geo-referenced polygon field
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, blank=True, default=None, null=True)
 
     def __str__(self):
         return "{} - Frame ({})".format(self.id, self.get_filename())
@@ -124,8 +95,9 @@ class Frame(models.Model):
 
 
 # Receive the pre_delete signal and delete the file associated with the model instance.
-@receiver(pre_delete, sender=Frame)
+@receiver(pre_delete, sender=ImageFrame)
 def video_delete(sender, instance, **kwargs):
     # Pass false so FileField doesn't save the model.
     if instance.content is not None:
         instance.content.delete(False)
+
